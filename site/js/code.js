a83bf82a64ed158811626d6d1758fb9249e73c61
Filip Torphage <filip.torphage@itggot.se>
2018-04-30 18:24:23 +0200

    tested layout on index

--- site/js/code.js
+++ site/js/code.js
@@ -1,7 +1,11 @@
 var clickbait = ['ic_airline_seat_individual_suite_black_24dp_1x.png', 'ic_disc_full_black_24dp_1x.png'];
 
 function randomImage(list){
-    alert(clickbait.keys())
     var num = Math.floor(Math.random() * list.length);
-    document.getElementById(clickbait.keys()).setAttribute('src', 'img/' + list[num]);
+    document.getElementById('clickbait').setAttribute('src', 'img/' + list[num]);
+}
+
+function toggleMobileMenu() {
+    var element = document.querySelector("nav")
+    element.classList.toggle("toggleMobileNav")
 }
