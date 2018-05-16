6f2c8ce724409f999e74902758c8b7c676c39b33
torphage <filip.torphage1@gmail.com>
2018-05-16 12:30:57 +0200

    Adjustments

--- site/public/js/code.js
+++ site/public/js/code.js
@@ -5,7 +5,29 @@ function randomImage(list){
     document.getElementById('clickbait').setAttribute('src', 'img/' + list[num]);
 }
 
-function toggleMobileMenu() {
-    var element = document.querySelector("nav")
-    element.classList.toggle("toggleMobileNav")
-}
+// toggleMenu.addEventListener('click',  function(event){
+    //     var element = document.querySelector("nav");
+    //     element.classList.toggle("toggleMobileNav")
+    // })
+    
+    
+const toggleMenu = document.getElementById('toggleMenu')
+toggleMenu.addEventListener('click', function(event){    
+    var element = document.querySelector("nav");
+    if (element.className !== 'togleMobileNav') {
+        element.classList.toggle("toggleMobileNav")        
+    } else {
+
+    }
+})
+
+const toggleMen = document.getElementById('index_container')
+toggleMen.addEventListener('click', function(event){    
+    var element = document.querySelector("nav");
+    var isClickInside = element.contains(event.target);
+    if (element.className === 'togleMobileNav') {
+        if (!isClickInside) {
+            element.classList.toggle("toggleMobileNav")
+        }
+    }
+})
