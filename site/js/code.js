43f98674a63e1724632b84f2e8f5f5bcebfb07f3
torphage <filip.torphage1@gmail.com>
2018-05-18 13:36:45 +0200

    made some changes

--- site/js/code.js
+++ site/js/code.js
@@ -1,14 +1,15 @@
-var clickbait = ['ic_airline_seat_individual_suite_black_24dp_1x.png', 'ic_disc_full_black_24dp_1x.png'];
 
-function randomImage(list){
-    var num = Math.floor(Math.random() * list.length);
-    document.getElementById('clickbait').setAttribute('src', 'img/' + list[num]);
+function randomIcon() {
+    var clickbait = ['menu', 'home'];
+    return clickbait[Math.floor((Math.random * 1.99))];
 }
+
+document.getElementById("clickbait").innerHTML = randomIcon();
 
 function toggleNav(){
     var element = document.querySelector("nav");
     element.classList.toggle("nav_active");
 
-    var temp = document.querySelector(".temp")
-    temp.classList.toggle("active")
+    var temp = document.querySelector(".temp");
+    temp.classList.toggle("active");
 }
